34b831e1817dae3b8617dd0a6f7793698b1b6d5f
Sergey Gerasimov <svlgerasimov@yandex.ru>
2022-09-23 09:56:16 +0300

    добавить в Entity конструкторы

--- src/main/java/com/example/jacksonexperiment/Entity.java
+++ src/main/java/com/example/jacksonexperiment/Entity.java
@@ -3,6 +3,15 @@ package com.example.jacksonexperiment;
 public class Entity {
     private int someField = 1;
 
+    public Entity() {
+        System.out.println("Constructor without args");
+        this.someField = 3;
+    }
+
+    public Entity(int someField) {
+        this.someField = someField;
+    }
+
     @Override
     public String toString() {
         return "Entity{" +
